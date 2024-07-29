d61ac60f9615f7ca6c7435413a2c220eeb67eefb
sunnyhakur666556@gmail.com <sunnythakur666556@gmail.com>
2024-07-29 21:12:32 +0530

    Async

---
+++ AsyncJS/async.js
@@ -0,0 +1,5 @@
+//Synchronous Javascript :
+// Javascript is a synchronous and single thread language . 
+// Execution context of JS is one line of code at a time .
+// Each operation waits for the last one to complete before executing.
+
