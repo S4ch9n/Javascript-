d1bcfff27b0814a070eb7913ac0319b99d8d1e3d
sunnyhakur666556@gmail.com <sunnythakur666556@gmail.com>
2024-07-29 09:23:47 +0530

    loops

--- Iteration/loops.js
+++ Iteration/loops.js
@@ -1,62 +1,61 @@
-// //for loop
+//for loop
 
-// for(let i = 0 ; i < 10 ; i++){
-//   // console.log(i);
-// } 
-
-
-// //loop with conditional
-// for(let i = 0 ; i < 10 ; i++){
-//   const ans = i 
-//   if(i == 5){
-//     console.log('I is equal',i);
-//   }
-//   console.log(ans);
-// }
-
-// //nested loop
-// for(let i = 0 ; i < 10 ; i++){
-//   console.log(`outer loop value ${i}`);
-//   for(let j = 0 ; j < 10 ; j++){
-//     console.log(`inner loop ${j}`);
-//   }
-// }
-
-// //print table 
-// for(let i = 0 ; i <= 10 ; i++){
-//   console.log(`Table of : ${i}`);
-//   for(let j = 1 ; j <= 10 ; j++){
-//     console.log(i + "*" + j + "=" + i*j);
-//   }
-// }
+for(let i = 0 ; i < 10 ; i++){
+  // console.log(i);
+} 
 
 
-// //array
-// const myArr = ["John" , "Doe" , "Nick"]
-// for(let i = 0 ; i < myArr.length ; i++){
-//   console.log(myArr[i]);
-// }
+//loop with conditional
+for(let i = 0 ; i < 10 ; i++){
+  const ans = i 
+  if(i == 5){
+  }
+  console.log(ans);
+}
+
+//nested loop
+for(let i = 0 ; i < 10 ; i++){
+  console.log(`outer loop value ${i}`);
+  for(let j = 0 ; j < 10 ; j++){
+    console.log(`inner loop ${j}`);
+  }
+}
+
+//print table 
+for(let i = 0 ; i <= 10 ; i++){
+  console.log(`Table of : ${i}`);
+  for(let j = 1 ; j <= 10 ; j++){
+    console.log(i + "*" + j + "=" + i*j);
+  }
+}
 
 
-// //break and continue
-// //break
-// for(let i = 0 ; i <= 10 ; i++){
-//   if(i == 3){
-//     console.log(`Detected 3`);
-//     break
-//   }
-//   console.log(`Value of i is : ${i}`);
-// }
+//array
+const myArr = ["John" , "Doe" , "Nick"]
+for(let i = 0 ; i < myArr.length ; i++){
+  console.log(myArr[i]);
+}
 
 
-// //continue
-// for(let i = 0 ; i <= 10 ; i++){
-//   if(i == 3){
-//     console.log(`Detected 3`);
-//     continue
-//   }
-//   console.log(`Value of i is : ${i}`);
-// }
+//break and continue
+//break
+for(let i = 0 ; i <= 10 ; i++){
+  if(i == 3){
+    console.log(`Detected 3`);
+    break
+  }
+  console.log(`Value of i is : ${i}`);
+}
+
+
+//continue
+for(let i = 0 ; i <= 10 ; i++){
+  if(i == 3){
+    console.log(`Detected 3`);
+    continue
+  }
+  console.log(`Value of i is : ${i}`);
+}
 
 
 //while loop 
