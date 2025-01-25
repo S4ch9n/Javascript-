425f736e12eef758ba5f9eaf96e72de8fbef2bc4
sunnyhakur666556@gmail.com <sunnythakur666556@gmail.com>
2025-01-25 11:44:41 +0530

    comparison in JS

--- Basic/comparison.js
+++ Basic/comparison.js
@@ -23,7 +23,11 @@ console.log('1' == 1 ); // true
 console.log('1' === 1); //false
 // in === operator , javascript  check the datatypes of both values , one is string and other is number so it will return false  
 
+
+//note that null become 0 , undefined become NaN
+
 //boolean 
+//Values that are intuitively “empty”, like 0, an empty string, null, undefined, and NaN, become false.
 console.log(true == 1);//true 
 console.log(true === 1);//false
 console.log(false == 0);//false
@@ -74,3 +78,29 @@ console.log(null >= 0)//true , null got convert into number , so 0 >= 0 , 0 is e
 console.log( undefined > 0 ); // false  , here undefined got convert into NaN and NaN is a special numeric value which returns false for all comparisons.
 console.log( undefined < 0 ); // false , here undefined got convert into NaN and NaN is a special numeric value which returns false for all comparisons.
 console.log( undefined == 0 ); //false , because null is only equal to undefined when using the == operator.
+
+
+
+
+
+//we can use Number(value)function to explicitly convert a value to a number
+// Assigning a string value "123" to the variable str
+let str = "123";
+
+// Logging the type of the variable str, which will be "string"
+console.log(typeof str); // Output: "string"
+
+// Converting the string "123" to a number using the Number() function
+let num = Number(str);
+
+// Logging the value of str, which is still the original string "123"
+console.log(str); // Output: "123"
+
+// Logging the type of the variable num, which is now a number
+console.log(typeof num); // Output: "number"
+
+
+alert( Number("   123   ") ); // 123
+alert( Number("123z") );      // NaN (error reading a number at "z")
+alert( Number(true) );        // 1
+alert( Number(false) );       // 0
