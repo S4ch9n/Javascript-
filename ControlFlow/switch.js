1b1dbd0d40420863855a69695c2ebb6fa23a8d39
sunnyhakur666556@gmail.com <sunnythakur666556@gmail.com>
2025-01-28 17:06:46 +0530

    switch in js

--- ControlFlow/switch.js
+++ ControlFlow/switch.js
@@ -1,11 +1,20 @@
-// switch (key) {
-//   case value:
+//basic syntax
+// switch(x) {
+//   case 'value1':  // if (x === 'value1')
+//     ...
+//     [break]
 
-//     break;
+//   case 'value2':  // if (x === 'value2')
+//     ...
+//     [break]
 
 //   default:
-//     break;
-// } this is the syntax of switch
+//     ...
+//     [break]
+// }
+//The value of x is checked for a strict equality to the value from the first case (that is, value1) then to the second (value2) and so on.
+// If the equality is found, switch starts to execute the code starting from the corresponding case, until the nearest break (or until the end of switch).
+// If no case is matched then the default code is executed (if it exists).
 
 
 const month = 4
@@ -54,4 +63,64 @@ switch (year) {
     break;
   default:
     console.log('no match');
+}
+
+//grouping of case : 
+let a = 3;
+switch (a) {
+  case 4:
+    console.log('Right!');
+    break;
+
+  case 3: // (*) grouped two cases
+  case 5:
+    console.log('Wrong!');
+    console.log("Why don't you take a math class?");
+    break;
+
+  default:
+    console.log('The result is strange. Really.');
+}
+
+
+//task - 1
+//Write the code using if..else which would correspond to the following switch:
+// switch (browser) {
+//   case 'Edge':
+//     alert( "You've got the Edge!" );
+//     break;
+
+//   case 'Chrome':
+//   case 'Firefox':
+//   case 'Safari':
+//   case 'Opera':
+//     alert( 'Okay we support these browsers too' );
+//     break;
+
+//   default:
+//     alert( 'We hope that this page looks ok!' );
+// }
+let browser = prompt("Please enter browser name: ");
+
+if (browser === 'Edge') {
+  alert("You've got the Edge!");
+} else if (browser === 'Chrome' || browser === 'Firefox' || browser === 'Safari' || browser === 'Opera') {
+  alert('Okay we support these browsers too');
+} else {
+  alert('We hope that this page looks ok!');
+}
+let input = +prompt("Enter a number: ");
+switch (input) {
+  case 0:
+    console.log(0);
+    break;
+  case 1:
+    console.log(1);
+    break;
+  case 2:
+  case 3:
+    console.log('2,3');
+    break;
+  default:
+    console.log("Not a valid value");
 }
