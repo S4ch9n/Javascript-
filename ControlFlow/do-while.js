576b6f3136cdfdc685679a7303570ece67568b31
sunnyhakur666556@gmail.com <sunnythakur666556@gmail.com>
2025-01-28 14:16:47 +0530

    do while in js

---
+++ ControlFlow/do-while.js
@@ -0,0 +1,8 @@
+//execute the body first , then check the condition , and while its truthy , execute again and again
+
+let i = 0;
+do{
+  console.log(i);
+  i++;
+}while(i < 10);
+//loop will run from 0 to 9
