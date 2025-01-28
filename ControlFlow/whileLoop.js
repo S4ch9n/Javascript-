43d63a23d8bc2cf9992a3535104a0c272594a095
sunnyhakur666556@gmail.com <sunnythakur666556@gmail.com>
2025-01-28 14:13:38 +0530

    while loop in js

---
+++ ControlFlow/whileLoop.js
@@ -0,0 +1,30 @@
+//loops are way to repeat the same code multiple times
+
+
+//basic syntax
+// while(condition){
+//   //code
+
+//   //so-called loop body
+// }
+
+//
+console.log("while loop for i ");
+let i = 0;
+while(i < 10){
+  console.log(i);
+  i++
+}
+
+//
+console.log("while loop for j ");
+let j = 5
+while(j > 0){
+  console.log(j);
+  j--;
+}
+
+//for single line of code we can write like this
+console.log("while loop for k ");
+let k = 1;
+while(k <= 10) console.log(k++);
