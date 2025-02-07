88c2fd4ace7f73d74af075474d23bd7c2f6f489c
sunnyhakur666556@gmail.com <sunnythakur666556@gmail.com>
2025-02-07 20:59:49 +0530

    datatypes in js

--- Basic/data_types.js
+++ Basic/data_types.js
@@ -67,15 +67,5 @@ console.log('Type of state is : ' + typeof state);
 
 
 
-//assignments
-let fname = "Rahul";
 
-// the expression is a number 1
-alert( `hello ${1}` ); // hello 1
-
-// the expression is a string "name"
-alert( `hello ${"name"}` ); // hello name
-
-// the expression is a variable, embed it
-alert( `hello ${fname}` ); // hello Rahul
 
