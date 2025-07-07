93402338eaacec06b3168d3f92d91111dfbe44af
sunnyhakur666556@gmail.com <sunnythakur666556@gmail.com>
2025-07-07 20:01:59 +0530

    temperature conversion

---
+++ mini-projects/TemperatureConversion/script.js
@@ -0,0 +1,27 @@
+const textBox = document.getElementById("textBox");
+
+const toFahrenheit = document.getElementById("toFahrenheit");
+
+const toClesius = document.getElementById("toClesius");
+
+const result = document.getElementById("result");
+
+let temp;
+
+
+
+function convert(){
+  if(toFahrenheit.checked){
+    temp = Number(textBox.value);
+    temp = temp * 9 / 5 + 32;
+    result.textContent =  temp.toFixed(1) + "°F";
+  }
+  else if(toClesius.checked){
+    temp = Number(textBox.value);
+    temp = (temp - 32) * (5 / 9);
+    result.textContent =  temp.toFixed(1) + "°F";
+  }
+  else{
+    result.textContent = "Select a unit";
+  }
+}
